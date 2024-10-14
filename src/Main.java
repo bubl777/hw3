32ae2baf00a4f7e543a627224aa7a68c13750812
buble <git@github.com:bubl777/helloworld.git>
2024-10-14 15:09:10 +0300

    сделал дз 3

---
+++ src/Main.java
@@ -0,0 +1,158 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 45000;
+        System.out.println("Значение переменной a с типом целочисленных равно " + a) ;
+        byte b = 44;
+        System.out.println("Значение переменной b с типом целочисленных равно " + b);
+        short c = 32000;
+        System.out.println("Значение переменной c с типом целочисленных равно " + c);
+        long d = 320000003;
+        System.out.println("Значение переменной d с типом целочисленных равно " + d);
+        float f = 3.76f;
+        System.out.println("Значение переменной f с типом с плавающой точкой равно " + f);
+        double g = 10.3d;
+        System.out.println(" Значение переменной g с типом с плавающой точкой равно " + g);
+        double F = 27.12d ;
+        System.out.println(F);
+        long AA = 987678965549L;
+        System.out.println(AA);
+        float B = 2.786f;
+        System.out.println(B);
+        short C = 569;
+        System.out.println(C);
+        short V = -159;
+        System.out.println(V);
+        short G = 27897;
+        System.out.println(G);
+        byte W = 67;
+        System.out.println(W);
+
+
+
+        int ListAll = 480;
+        int LP = 23;
+        int AS = 27;
+        int EA = 30;
+        int ALL = LP + AS +EA;
+        System.out.println(" всего учеников " + ALL);
+        int Student = ListAll / ALL;
+        System.out.println(" На каждого ученика расчитано " + Student + " листов бумаги ");
+        int botl = 8;
+        int timeOne = 20;
+        int timeTwo = 1440;
+        int timeThree = 4320;
+        int timeFour = 43200000;
+        int TimeOne = botl * timeOne;
+        System.out.println(" за " + timeOne + " минут, машина произвела " + TimeOne + " штук бутылок ");
+        int TimeTwo = botl * timeTwo;
+        System.out.println(" за " + timeTwo + " минут, машина произвела " + TimeTwo + " штук бутылок ");
+        int TimeThree = botl * timeThree;
+        System.out.println(" за " + timeThree + " минут, машина произвела " + TimeThree + " штук бутылок ");
+        int TimeFour = botl * timeFour;
+        System.out.println(" за " + timeFour + " минут, машина произвела " + TimeFour + " штук бутылок ");
+        int AllBanok = 120;
+        int White = 2;
+        int  Brown = 4;
+        int WhiteBrown = White + Brown;
+        System.out.println(WhiteBrown);
+        int ALl = AllBanok / WhiteBrown;
+        System.out.println(ALl);
+        int WhiteTwo = ALl * White;
+        System.out.println(WhiteTwo);
+        int BrownTwo = ALl * Brown;
+        System.out.println(BrownTwo);
+        int AP = AllBanok / WhiteBrown;
+        System.out.println(AP);
+        System.out.println(" В школе , где " + AP + " классов , нужно " + WhiteTwo + " банок белой краски и " + BrownTwo + " банок коричневой кркаски ");
+        int Banana = 80;
+        int banan = 5;
+        int banans = Banana * banan;
+        System.out.println(" Бананы " + banans);
+        int milk = 105;
+        int milks = 2;
+        int milkss = milk * milks;
+        System.out.println(" молоко " + milkss);
+        int ice = 100;
+        int ice2 =  2;
+        int iceCream = ice * ice2;
+        System.out.println(" мороженое " + iceCream );
+        int egg = 70;
+        int egg2 = 4;
+        int eggs = egg2 * egg;
+        System.out.println(" яйца " + eggs);
+        float allALL = milkss + iceCream + eggs + banans;
+        System.out.println(" спортзавтрак " + allALL + " грамм ");
+        float FF = allALL / 1000;
+        System.out.println( " в кг  " + FF);
+        int kg = 7;
+        int gr = kg * 1000;
+        System.out.println(" грамм " + gr) ;
+        int loseWeight = 250;
+        int loseWeightt = 500;
+        int day = gr / loseWeight;
+        int dayTwo = gr / loseWeightt;
+        System.out.println(" сколько дней потребовалось для  похудения на 250 грамм "  + day );
+        System.out.println(" сколько дней потребовалось для похудения на 500 грамм  " + dayTwo);
+        float masha = 67760;
+        float denis = 83690;
+        float kristina = 76230;
+        float procent =  0.1f;
+        float mashaOne = procent * masha;
+        System.out.println(" процент повышения маши " + mashaOne);
+        float denosOne = denis * procent;
+        System.out.println(" процент повышения дениса " + denosOne);
+        float kristinaOne = kristina * procent ;
+        System.out.println(" процент повышения кристины " + kristinaOne);
+        float mashaTwo =  masha + mashaOne ;
+        System.out.println(" маша после повышения " + mashaTwo);
+        float denisTwo =  denis + denosOne ;
+        System.out.println(" денис после повышения " + denisTwo );
+        float kristinaTwo =  kristina + kristinaOne;
+        System.out.println(" кристина после повышения " + kristinaTwo );
+        float mashaYaersOne = masha * 12;
+        System.out.println(" за первый год работы Маши " + mashaYaersOne );
+        float mashaYaersTwo =  mashaTwo * 12 ;
+        System.out.println(" после повышение зарабаток за год у Марии " + mashaYaersTwo );
+        float mashaDifference = mashaYaersTwo - mashaYaersOne;
+        System.out.println(" разница после повышение зарплаты Марии " + mashaDifference);
+        float denisYearsOne = denis * 12;
+        System.out.println(" за первый год работы Дена " + denisYearsOne );
+        float denisYaersTwo = denisTwo * 12;
+        System.out.println(" после повышение зарабаток за год у Дениса " + denisYaersTwo );
+        float denisDifference = denisYaersTwo - denisYearsOne;
+        System.out.println(" разница после повышение зарплаты Дениса " + denisDifference);
+        float kristinaYearsOne = kristina * 12;
+        System.out.println(" за первый год работы Кристины " + kristinaYearsOne );
+        float kristinaYaersTwo = kristinaTwo * 12;
+        System.out.println(" после повышение зарабаток за год у Кристины " + kristinaYaersTwo );
+        float kristinaDifference = kristinaYaersTwo - kristinaYearsOne;
+        System.out.println(" разница после повышение зарплаты Кристины " + kristinaDifference );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+    }
